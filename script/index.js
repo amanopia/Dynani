328574a8a3d8b7d5885dea49f276f8da46987ed7
unknown <amanvermaspace@gmail.com>
2023-01-03 11:50:10 +0530

    tested the feature of highlighting an element if present in a sorted array. Feature being tested for binary search

---
+++ script/index.js
@@ -0,0 +1,26 @@
+let target = window.prompt("Enter target: ")
+
+let arr = [3,6,7,8,10,13];
+// console.log(binarySearch(arr, target));
+function binarySearch(arr, target){
+    let start = 0;
+    let end = arr.length - 1;
+
+    while(start <= end){
+        let mid = Math.trunc(start + (end-start)/2);
+        if(target > arr[mid]){
+            start = mid + 1;
+        } else if (target < arr[mid]){
+            end = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return -1;
+}
+
+let targetIndex = binarySearch(arr, target);
+console.log(binarySearch(arr, target));
+let element1 = document.querySelector(`.el${targetIndex+1}`);
+element1.style.border = "4px solid red"
